473ff85563e3053e49e7f31970eb0e4285540587
Alesia1216 <149078855+Alesia1216@users.noreply.github.com>
2025-04-07 10:03:44 +0200

    Update Program.cs

--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -14,7 +14,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilderABC(args);
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOutputCache(opciones =>
 {
